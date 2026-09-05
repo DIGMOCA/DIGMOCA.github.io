7773033e117d28fd3b98b49ce0cc020159ebff09
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-06 01:21:03 +0900

    Add link to SERIES in the navigation

--- header.js
+++ header.js
@@ -16,6 +16,7 @@ header.innerHTML = `
     <nav>
       <a href="${basePath}index.html">WORKS</a>
       <a href="${basePath}years.html">YEARS</a>
+      <a href="${basePath}series.html">SERIES</a>
       <a href="${basePath}about.html">ABOUT</a>
       <a href="https://lit.link/digmoca">SNS</a>
     </nav>
