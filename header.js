1efc2a100fb10e89563f6cd81bf6191e4030f449
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-05 22:22:33 +0900

    Create header.js

---
+++ header.js
@@ -0,0 +1,24 @@
+const header = document.getElementById("site-header");
+
+const isWorkPage =
+  window.location.pathname.includes("/works/");
+
+const basePath =
+  isWorkPage ? "../" : "";
+
+header.innerHTML = `
+  <header>
+
+    <h1>
+      DIGMOCA ARCHIVE
+    </h1>
+
+    <nav>
+      <a href="${basePath}index.html">WORKS</a>
+      <a href="${basePath}years.html">YEARS</a>
+      <a href="${basePath}about.html">ABOUT</a>
+      <a href="https://lit.link/digmoca">SNS</a>
+    </nav>
+
+  </header>
+`;
