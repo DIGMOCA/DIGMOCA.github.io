77a1051ddae38d801877445cf0b52e3758e98b7e
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-06 01:40:12 +0900

    Update song names and orders in series-data.js

--- series-data.js
+++ series-data.js
@@ -8,11 +8,31 @@ const seriesData = [
     order: 2
   },
   {
-    name: "無色透名美術館",
+    name: "オリジナル曲β",
     order: 3
   },
-  {
-    name: "感情動作図鑑",
+　{
+    name: "オリジナル曲γ",
     order: 4
+  },
+　{
+    name: "オリジナル曲δ",
+    order: 5
+  },
+　{
+    name: "オリジナル曲ε",
+    order: 6
+  },
+  　{
+    name: "オリジナル曲ζ",
+    order: 7
+  },
+  {
+    name: "無色透名美術館",
+    order: 8
+  },
+  {
+    name: "未分類",
+    order: 9
   }
 ];
