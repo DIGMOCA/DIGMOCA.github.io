e18546e39e84b8b0810d0e50dbce7e67a4006a79
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-05 17:24:28 +0900

    Remove two entries from works-data.js
    Removed two works entries from the works-data.js file.

--- works-data.js
+++ works-data.js
@@ -1,22 +1,3 @@
-  {
-    title: "【無色透名美術館】ひ　い　　て　　　い　　　　く　　　　　　。",
-    date: "2026-07-23",
-    categories: ["illustration"],
-    image: "images/musyokutoumei3_5.jpg",
-    page: "works/musyokutoumei3_5.html"
-  }
-
-    {
-    title: "【無色透名美術館】とか、いわな",
-    date: "2026-05-24",
-    categories: ["illustration"],
-    image: "images/musyokutoumei3_4.jpg",
-    page: "works/musyokutoumei3_4.html"
-  }
-
-];
-
-
 const works = [
 
   {
