f9caf403961674c5d2fdd974764a5810f199b7cd
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-06 01:08:30 +0900

    Add new works to works-data.js

--- works-data.js
+++ works-data.js
@@ -1,41 +1,51 @@
 const works = [
 
   {
+    id: "musyokutoumei3_5",
     title: "【無色透名美術館】ひ　い　　て　　　い　　　　く　　　　　　。",
     date: "2026-07-23",
     categories: ["illustration"],
+    series: ["無色透名美術館"],
     image: "images/musyokutoumei3_5.jpg",
     page: "works/musyokutoumei3_5.html"
   },
 
   {
+    id: "musyokutoumei3_4",
     title: "【無色透名美術館】とか、いわな",
     date: "2026-05-24",
     categories: ["illustration"],
+    series: ["無色透名美術館"],
     image: "images/musyokutoumei3_4.jpg",
     page: "works/musyokutoumei3_4.html"
   },
 
   {
+    id: "musyokutoumei3_3",
     title: "【無色透名美術館】黄昏に染まる",
     date: "2026-05-02",
     categories: ["illustration"],
+    series: ["無色透名美術館"],
     image: "images/musyokutoumei3_3.jpg",
     page: "works/musyokutoumei3_3.html"
   },
 
-    {
+  {
+    id: "animation_1",
     title: "くるくる",
     date: "2023-10-30",
     categories: ["animation"],
+    series: [],
     image: "images/animation_1.jpg",
     page: "works/animation_1.html"
   },
 
-    {
+  {
+    id: "vocaloid_1",
     title: "MIZU・UMI / 東北きりたん",
     date: "2022-10-08",
     categories: ["music"],
+    series: ["VOCALOID楽曲", "オリジナル曲α"],
     image: "images/vocaloid_1.jpg",
     page: "works/vocaloid_1.html"
   },
