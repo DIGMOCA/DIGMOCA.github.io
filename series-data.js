95b7f8402f151aa6d19423bde86d65acd3874100
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-06 01:29:33 +0900

    Add series-data.js with initial series data

---
+++ series-data.js
@@ -0,0 +1,18 @@
+const seriesData = [
+  {
+    name: "ボーカロイド楽曲",
+    order: 1
+  },
+  {
+    name: "オリジナル曲α",
+    order: 2
+  },
+  {
+    name: "無色透名美術館",
+    order: 3
+  },
+  {
+    name: "感情動作図鑑",
+    order: 4
+  }
+];
