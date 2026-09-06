ad58d4306bd751a9b6f0e4183c7444d14279ee64
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-06 23:44:59 +0900

    Update works-data.js

--- works-data.js
+++ works-data.js
@@ -422,7 +422,7 @@ const works = [
           content: [
       {
         type: "soundcloud",
-        embedUrl: "ここにSoundCloudの埋め込みURL",
+        embedUrl: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1624809621&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
       },
       {
         type: "text",
