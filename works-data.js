fd58491d1283e1f6abb7a6436fece4a9924ee468
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-06 12:33:35 +0900

    Add content section with links and descriptions

--- works-data.js
+++ works-data.js
@@ -28,6 +28,31 @@ const works = [
     series: ["無色透名美術館"],
     image: "images/musyokutoumei3_3.jpg",
     page: "works/musyokutoumei3_3.html"
+
+    content: [
+  {
+    type: "text",
+    text: "無色透名祭３で投稿された「黄昏に染まる / 重音テト」のファンアートです。"
+  },
+    {
+    type: "text-link",
+    before: "素敵な楽曲は",
+    label: "こちら",
+    url: "https://www.nicovideo.jp/watch/so45511807",
+    after: ""
+  },
+   {
+    type: "link",
+    label: "Pixiv",
+    url: "https://www.pixiv.net/artworks/144257619"
+  },
+ {
+    type: "link",
+    label: "無色透名祭３",
+    url: "https://site.nicovideo.jp/mushokutomeisai/2025/"
+  }
+
+  
   },
 
     {
