590b9c25f0dcef0488b8b1ad60fa884663f0b2cd
DIGMOCA <thisismylastwar@outlook.jp>
2026-09-07 00:28:06 +0900

    Update series-data.js

--- series-data.js
+++ series-data.js
@@ -32,7 +32,39 @@ const seriesData = [
     order: 8
   },
     {
-    name: "未分類",
+    name: "時刻シリーズ",
     order: 9
+  },
+    {
+    name: "季節シリーズ",
+    order: 10
+  },
+    {
+    name: "シーンシリーズ",
+    order: 11
+  },
+    {
+    name: "月シリーズ",
+    order: 12
+  },
+    {
+    name: "イベントシリーズ",
+    order: 13
+  },
+      {
+    name: "ピアプロイラスト",
+    order: 14
+  },
+        {
+    name: "1ページ漫画",
+    order: 15
+  },
+  {
+    name: "制作協力",
+    order: 16
+  },
+    {
+    name: "未分類",
+    order: 17
   }
 ];
